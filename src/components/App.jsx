d3544ecfa383a3ca39e30abc77a8dabbfd59b374
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2023-01-31 15:13:13 +0200

    update hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,64 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactFrom/ContactFrom';
 import { Filter } from './Filter/Filter';
 import { Title, Box, TitleContacts } from './App.styled';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
 
-    filter: '',
-  };
-  componentDidMount() {
-    const pasedContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (pasedContacts) {
-      this.setState({
-        contacts: pasedContacts,
-      });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.setState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  addContact = ({ name, number }) => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-    const isExist = this.state.contacts.find(contact => contact.name === name);
+    const isExist = contacts.find(contact => contact.name === name);
     if (isExist !== undefined) {
       return alert(`${name} is already in contacts`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [...contacts, contact],
-      }));
+      setContacts([...contacts, contact]);
     }
   };
-  changeFilter = evt => {
-    this.setState({ filter: evt.currentTarget.value });
+  const changeFilter = evt => {
+    setFilter(evt.currentTarget.value);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
-  render() {
-    const { getVisibleContacts, changeFilter, addContact, deleteContact } =
-      this;
-    const { filter } = this.state;
-    return (
-      <Box>
-        <Title>Phonebook</Title>
-        <ContactForm onSubmit={addContact} />
-        <TitleContacts>Contacts</TitleContacts>
-        <Filter changeFilter={changeFilter} filter={filter} />
-        <ContactList
-          deleteContact={deleteContact}
-          contacts={getVisibleContacts()}
-        />
-      </Box>
-    );
-  }
-}
+
+  return (
+    <Box>
+      <Title>Phonebook</Title>
+      <ContactForm onSubmit={addContact} />
+      <TitleContacts>Contacts</TitleContacts>
+      <Filter changeFilter={changeFilter} filter={filter} />
+      <ContactList
+        deleteContact={deleteContact}
+        contacts={getVisibleContacts()}
+      />
+    </Box>
+  );
+};
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,20 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ListItem, List } from './ContactList.styled';
-export class ContactList extends Component {
-  render() {
-    const { contacts, deleteContact } = this.props;
-    return (
-      <List>
-        {contacts.map(({ id, name, number }) => (
-          <ListItem key={id}>
-            {name}: {number}
-            <button onClick={() => deleteContact(id)} type="button">
-              Delete
-            </button>
-          </ListItem>
-        ))}
-      </List>
-    );
-  }
-}
+export const ContactList = ({ contacts, deleteContact }) => {
+  return (
+    <List>
+      {contacts.map(({ id, name, number }) => (
+        <ListItem key={id}>
+          {name}: {number}
+          <button onClick={() => deleteContact(id)} type="button">
+            Delete
+          </button>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
 ContactList.propeTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
   deleteContact: PropTypes.func.isRequired,
